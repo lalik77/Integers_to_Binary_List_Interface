f092107fbd6d0c9288c1fa3ea241534162ff4dd1
Alexander Mami <45859153+lalik77@users.noreply.github.com>
2019-12-15 01:26:17 +0300

    Update Int_to_Binary.java

--- Int_to_Binary.java
+++ Int_to_Binary.java
@@ -5,7 +5,25 @@ import java.util.List;
 public class Main {
 
 public static void main(String[] args) {
-}
+  
+  List<Integer>  list = Arrays.asList(5,3,7,10,14);
+  List<String> list_str = new ArrayList<String>();
+  
+  System.out.println("List of integers on decimal  " + list.toString());
+  
+   for (int i=0;i<list.size();i++) {
+           
+            int decimal=list.get(i);
+            String str=  Integer.toBinaryString(decimal);                     
+            list_str.add(str);
+
+        }
+  
+  
+  System.out.println("List of integers on binary " + list.toString());
+
+
+      }
 
 
 }
